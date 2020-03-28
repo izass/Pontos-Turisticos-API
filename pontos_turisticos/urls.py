b6c912e93285d247929bb35b031fb192b61e8959
isaac <izass1312@gmail.com>
2020-03-27 23:36:03 -0300

    Register of system apps routes

--- pontos_turisticos/urls.py
+++ pontos_turisticos/urls.py
@@ -18,10 +18,18 @@ from django.conf.urls import include
 from django.urls import path
 from rest_framework import routers
 from core.api.viewsets import PontoTuristicoViewSet
-
+from avaliacoes.api.viewsets import AvaliacoesViewSet
+from atracoes.api.viewsets import AtracoesViewSet
+from enderecos.api.viewsets import EnderecoViewSet
+from comentarios.api.viewsets import ComentarioViewSet
 
 router = routers.DefaultRouter()
-router.register(r'pontoturistico', PontoTuristicoViewSet)
+router.register(r'pontosturisticos', PontoTuristicoViewSet)
+router.register(r'avaliacoes', AvaliacoesViewSet)
+router.register(r'atracoes', AtracoesViewSet)
+router.register(r'enderecos', EnderecoViewSet)
+router.register(r'comentarios', ComentarioViewSet)
+
 
 urlpatterns = [
     path('', include(router.urls)),
