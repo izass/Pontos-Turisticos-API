c60c99e626b29ecca5f8efecb3e4f0ae0f0e64e9
isaac <izass1312@gmail.com>
2020-04-07 21:10:36 -0300

    Overview of djanngo restframework methods

--- core/api/viewsets.py
+++ core/api/viewsets.py
@@ -1,6 +1,9 @@
 from rest_framework.viewsets import ModelViewSet
 from core.models import PontoTuristico
 from .serializers import PontoTuristicoSerializer
+from rest_framework.decorators import action
+from rest_framework.response import Response
+
 
 class PontoTuristicoViewSet(ModelViewSet):
     """
@@ -9,3 +12,28 @@ class PontoTuristicoViewSet(ModelViewSet):
     serializer_class = PontoTuristicoSerializer
     def get_queryset(self):
         return PontoTuristico.objects.filter(aprovado=True)
+
+    ###metodos drf a sobescrever###
+
+    # def list(self, request, *args, **kwargs):
+    #     return Response({'teste': 123})
+
+    # def create(self, request, *args, **kwargs):
+    #     return Response({'hello': request.data})
+
+    # def destroy(self, request, *args, **kwargs):
+    #     pass
+
+    # def retrieve(self, request, *args, **kwargs):
+    ## to do the default behavior of http verb
+    #     return super(PontoTuristicoViewSet, self).update(request, *args, **kwargs)
+
+    # def update(self, request, *args, **kwargs):
+    #     pass
+
+    # def partial_update(self, request, *args, **kwargs):
+    #     pass
+
+    # @action(methods=['get'], detail=True)
+    # def denunciar(self, request, pk=None):
+    #     pass
